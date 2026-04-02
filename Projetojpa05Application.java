0068bcac5815530afc39c616b9d2e12f6f8664e7
GUSTAVO CARVALHO <carvalhogustavo944@gmail.com>
2026-04-02 15:24:22 -0300

    APPLICATION

---
+++ Projetojpa05Application.java
@@ -0,0 +1,13 @@
+package com.projetojpa;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Projetojpa05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Projetojpa05Application.class, args);
+	}
+
+}
